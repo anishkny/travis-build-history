d075484352ed4150a8f85aad2179fa6e62dd8230
Anish Karandikar <anishkny@gmail.com>
2018-10-08 10:57:58 -0400

    chore: Add .travis.yml

--- test/test.js
+++ test/test.js
@@ -1,22 +1,18 @@
 const sut = require('../index.js');
 const sinon = require('sinon');
 
-describe('Smoke', async () => {
+it('should get build history', async () => {
+  const res = createMockResponseHandler();
+  await sut.history({ query: { repo: 'eugeneware/gifencoder' } }, res);
+  console.log(res.status.args[0]);
+  console.log(res.send.args[0]);
+});
 
-  it('should get build history', async () => {
-    const res = createMockResponseHandler();
-    await sut.history({ query: { repo: 'eugeneware/gifencoder' } }, res);
-    console.log(res.status.args[0]);
-    console.log(res.send.args[0]);
-  });
-
-  it('should return 404 for unknown repo', async () => {
-    const res = createMockResponseHandler();
-    await sut.history({ query: { repo: 'anishkny/unknown-f3d86dbcd73a' } }, res);
-    console.log(res.status.args[0]);
-    console.log(res.send.args[0]);
-  });
-
+it('should return 404 for unknown repo', async () => {
+  const res = createMockResponseHandler();
+  await sut.history({ query: { repo: 'anishkny/unknown-f3d86dbcd73a' } }, res);
+  console.log(res.status.args[0]);
+  console.log(res.send.args[0]);
 });
 
 function createMockResponseHandler() {
